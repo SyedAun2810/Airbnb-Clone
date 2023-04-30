5b81f292f7d3166c6cd9846d7edfa1496375b05f
Khushbakht Khan <103233325+khushbakhtkhan1@users.noreply.github.com>
2023-04-30 09:51:27 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,26 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Gridd from "./components/Gridd";
 import Cards from "./components/Cards";
+import data from "./data";
 
 const App=()=>{
+    const cards=data.map(item=>{
+        return(
+            <Cards 
+                img={item.img}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    })
     return(
         <div>
             <Navbar />
             <Gridd />
-            <Cards 
-                img="katie-zaferes.jpeg"
-                rating="5.0"
-                reviewCount={6}
-                country="USA"
-                title="Life Lessons with Katie Zaferes"
-                price={136}
-            />
+            {cards}
         </div>
     )
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -10,7 +10,7 @@ const Cards=(props)=>{
                 <span className="gray">
                     ({ props.reviewCount })    
                 </span>
-                <span className="gray"> { props.country }</span>
+                <span className="gray"> { props.location }</span>
             </div>
             <p>{props.title}</p>
             <p><span className="bold">From ${props.price}</span> / person</p>
